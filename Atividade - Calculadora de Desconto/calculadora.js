40b08248698fcba2195bd92f45f7e7abec4172bf
Matheus Gouveia <matheusgouveiadev42@gmail.com>
2025-03-14 18:00:01 -0300

    Add files via upload

---
+++ Atividade - Calculadora de Desconto/calculadora.js
@@ -0,0 +1,16 @@
+function calcularDesconto(){
+
+    //Entrada
+        let valorOriginal = document.getElementById("valor").value;
+        let desconto = document.getElementById("desconto").value;
+
+    //Processamento
+        let valorDesconto = (valorOriginal * desconto) / 100;
+        let valorFinal = valorOriginal - valorDesconto;
+
+    //Saída
+
+         document.getElementById("resultado").textContent = "Valor Final: " + valorFinal;
+
+}
+
