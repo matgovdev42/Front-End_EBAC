33a00ba684cbdf4689b5ded0a46031dd64f4a888
Matheus Gouveia <matheusgouveiadev42@gmail.com>
2025-04-28 16:25:00 -0300

    Add files via upload

---
+++ Teste de CODE JS/teste1.js
@@ -0,0 +1,28 @@
+//Define uma condição simples
+
+if(numero > 0){
+    console.log("O número é positivo");
+}
+
+//Define uma condição de if...else
+
+let idade = 18;
+if(idade >= 18){
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
+
+//Define multiplas condições
+
+const nota = 7;
+if (nota >= 9) {
+    console.log("Parabéns! Você tirou A");
+} else if (nota >= 7){
+    console.log("Boa! Você tirou B");
+} else if (nota >= 5){
+    console.log("Você tirou C, estudo mais.");
+} else {
+    console.log("Reprovado!");
+}
+
